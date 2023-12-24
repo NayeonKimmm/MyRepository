c83cfb7e1fb62facd9cff74425477283d3761843
User <User@DESKTOP-RDQD5BL>
2023-12-24 11:53:18 +0900

    A.java/B.java/C.java 수정

---
+++ GitHW/src/mygit/A.java
@@ -0,0 +1,10 @@
+package mygit;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHW");
+		System.out.println("This is my 2nd GitHW!");
+	}
+
+}
---
+++ GitHW/src/mygit/C.java
@@ -0,0 +1,9 @@
+package mygit;
+
+public class C {
+	public void sayGoodMorning(String toWhom) {
+		System.out.println("GoodMorning! "+ toWhom);
+		System.out.println("Your change in master branch");
+	}
+
+}

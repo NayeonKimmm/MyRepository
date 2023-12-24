5e3ef98ad7d63a782091bd2a4fa2bc328785ab17
User <User@DESKTOP-RDQD5BL>
2023-12-24 11:59:47 +0900

    A.java 수정

--- GitHW/src/mygit/A.java
+++ GitHW/src/mygit/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHW");
 		System.out.println("This is my 2nd GitHW!");
+		System.out.println("Modify Class");
 	}
 
 }

c9960dcd6246e42e8fa2bc2e7afa44fba8a22786
User <User@DESKTOP-RDQD5BL>
2023-12-24 12:06:43 +0900

    A.java/C.java 수정

--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -3,7 +3,7 @@ package mygit;
 public class C {
 	public void sayGoodMorning(String toWhom) {
 		System.out.println("GoodMorning! "+ toWhom);
-		System.out.println("Your change in master branch");
+		System.out.println("Have a nice Holiday!");
 	}
 
 }
